e6644883885e6dba86624d9b6606ba0a49f87cf0
zhangsan <zhangsan@163.com>
2020-12-07 17:40:18 +0800

    开发人员更新的内容

--- git/src/main/java/com/bjpowernode/git/Test.java
+++ git/src/main/java/com/bjpowernode/git/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("============Hello world!=========");
         System.out.println("=======项目经理再次更新内容======");
+        System.out.println("==========开发人员更新的代码===========");
     }
 }
