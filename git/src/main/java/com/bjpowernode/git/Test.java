2b8309b123842fecb0ca0e7895c1d2a03fca1799
zhangsan <zhangsan@163.com>
2020-12-07 17:32:06 +0800

    项目经理再次更新内容

--- git/src/main/java/com/bjpowernode/git/Test.java
+++ git/src/main/java/com/bjpowernode/git/Test.java
@@ -6,5 +6,6 @@ package com.bjpowernode.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("============Hello world!=========");
+        System.out.println("=======项目经理再次更新内容======");
     }
 }
