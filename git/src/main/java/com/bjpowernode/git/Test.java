200021f40e17d9743964634877fdba4527e29386
zhangsan <zhangsan@163.com>
2020-12-07 16:21:15 +0800

    提交整个项目

---
+++ git/src/main/java/com/bjpowernode/git/Test.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.git;
+
+/**
+ *
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("============Hello world!=========");
+    }
+}
